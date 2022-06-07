56a3016c02aed43c1365a72fb3efe3a065dd5d1c
Nick D'Esposito <Nicholas D'Esposito>
2022-06-07 19:11:47 -0400

    minor variable changes. Still need to work on eventlistener

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 let mainDiv = document.getElementById("main");
 let fighter = null;
 let playableArea = document.getElementById("playable_area");
-
+let character1 = document.getElementById("john");
 function placeCharacter(character) {
     playableArea.append(document.getElementById(character));
 }
+
+let johnSelected = document.querySelector("#john");
+let lisaSelected = document.querySelector("#lisa");
+let philSelected = document.querySelector("#phil");
+let jenSelected = document.querySelector("#jen");
+
 function createSelection(character) {
-    const selectedCharacter = document.getElementById(character);
-    selectedCharacter.addEventListener("click", function () {
-        var characterSelected = selectedCharacter;
-        if ((characterSelected = "lisa")) {
-            console.log(selectedCharacter);
-        } else if ((characterSelected = "john")) {
-            console.log(selectedCharacter);
-        } else {
-            console.log("else");
-        }
+    character.addEventListener("click", function (character) {
+        // var characterSelected = selectedCharacter;
+        // if ((characterSelected = "lisa")) {
+        //     console.log(selectedCharacter);
+        // } else if ((characterSelected = "john")) {
+        //     console.log(selectedCharacter);
+        // } else {
+        //     console.log("else");
+        // }
 
         //  switch (characterSelected) {
         //      case "lisa":
@@ -27,10 +32,15 @@ function createSelection(character) {
         //     default:
         //        break;
         //  }
-        if ((selectedCharacter.id = "john")) {
+        if ((character.id = "john")) {
             fighter = "john";
             window.location.href = "gamescreen.html";
-        } else if ((selectedCharacter.id = "lisa")) {
+            //prettier-ignore
+            document.playableArea.appendChild("Pixel-Art JS-Game/John/John-Nuetral.png");
+            character1.style.width("10vw");
+            character1.style.height("10vh");
+            character1.style.zIndex("1");
+        } else if ((character.id = "lisa")) {
             fighter = "lisa";
             window.location.href = "gamescreen.html";
         } else {
@@ -40,6 +50,6 @@ function createSelection(character) {
         return fighter;
     });
 }
-const lisa_selection = createSelection("lisa");
+const lisa_selection = createSelection(lisaSelected);
 
-const john_selection = createSelection("john");
+const john_selection = createSelection(johnSelected);
