0cfe7ada0b0da8b72ecb1c0d47b53ba40cf17c3e
Nick D'Esposito <Nicholas D'Esposito>
2022-06-10 06:41:05 -0400

    Still Working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let mainDiv = document.getElementById("main");
 let fighter = null;
 let playableArea = document.getElementById("playable_area");
+var direction = null,
+    x = 680,
+    y = -250;
 
 function placeCharacter(character) {
     playableArea.append(document.getElementById(character));
@@ -18,33 +21,30 @@ let philSelected = document.querySelector("#phil");
 let jenSelected = document.querySelector("#jen");
 
 function createSelection(character) {
-    let selectedCharacter = document.getElementById("john");
+    let selectedCharacter = document.getElementById(character);
     console.log(selectedCharacter);
     console.log(character);
     selectedCharacter.addEventListener("click", function (character) {
-        // var characterSelected = selectedCharacter;
-        // if ((characterSelected = "lisa")) {
-        //     console.log(selectedCharacter);
-        // } else if ((characterSelected = "john")) {
-        //     console.log(selectedCharacter);
-        // } else {
-        //     console.log("else");
-        // }
-
-        //  switch (characterSelected) {
-        //      case "lisa":
-        //          console.log("lisa");
-        //          return characterSelected;
-        //      case "john":
-        //          console.log("john");
-        //          return characterSelected;
-        //     default:
-        //        break;
-        //  }
-        if ((character.id = "john")) {
+        if (character.id == "john") {
             placeCharacter("john");
             selectionDisappear();
-        } else if ((character.id = "lisa")) {
+            selectedCharacter.style.left = x + "px";
+            selectedCharacter.style.bottom = y + "px";
+        } else if (character.id == "lisa") {
+            placeCharacter("lisa");
+            selectionDisappear();
+            selectedCharacter.style.left = x + "px";
+            selectedCharacter.style.bottom = y + "px";
+        } else if (character.id == "phil") {
+            placeCharacter("phil");
+            selectionDisappear();
+            selectedCharacter.style.left = x + "px";
+            selectedCharacter.style.bottom = y + "px";
+        } else if (character.id == "jen") {
+            placeCharacter("lisa");
+            selectionDisappear();
+            selectedCharacter.style.left = x + "px";
+            selectedCharacter.style.bottom = y + "px";
         } else {
             mainDiv.innerHTML("Error");
         }
@@ -57,14 +57,35 @@ function selectionDisappear() {
     mainDiv.style.opacity = "0";
 }
 
-johnSelected.addEventListener("keydown", checkKeyPress, false);
-function checkKeyPress(key) {
-    if (key.keyCode == "87") {
-        movingThing.style.bottom += "10px";
-        console.log("w pressed");
-    }
-}
+// johnSelected.addEventListener("keydown", checkKeyPress, false);
+// function checkKeyPress(key) {
+//     if (key.keyCode == "87") {
+//         movingThing.style.bottom += "10px";
+//         console.log("w pressed");
+//     }
+// }
 
+johnSelected.addEventListener("keydown", function (e) {
+    if (e.repeat) return;
+    if (e.keycode === "87") {
+        direction = "north";
+        y += 10;
+    }
+    if (e.keycode === "65") {
+        direction = "west";
+        x -= 10;
+    }
+    if (e.keycode === "83") {
+        direction = "south";
+        y -= 10;
+    }
+    if (e.keycode === "68") {
+        direction = "east";
+        x += 10;
+    }
+});
+
+//https://github.com/NickDespo05/JS-Web-Game-Part-7.git
 //https://www.youtube.com/watch?v=VBVOXTgXX2U
 
 const lisa_selection = createSelection("lisa");
